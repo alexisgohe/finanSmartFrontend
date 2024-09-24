5975d5c718c894b24c2bc7d5dc68d76dc9217ce4
Alexis Gómez <agomezh@veracruz.gob.mx>
2024-09-24 16:15:48 -0600

    FIX - Se trabaja con el diseño del módulo de ingresos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule  } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './modules/login/login.component';
 import { DashboardComponent } from './modules/dashboard/dashboard.component';
 import { HttpClientModule } from '@angular/common/http';
-import { LoginService } from './service/login.service';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { HTTP_INTERCEPTORS } from '@angular/common/http';
 import { AuthInterceptor } from './guards/auth.interceptor';
 import { SidebarComponent } from './core/sidebar/sidebar.component';
 import { HeaderComponent } from './core/header/header.component';
+import { CommonModule } from '@angular/common';
+import { IngresosComponent } from './modules/ingresos/ingresos.component';
+import { CategoriasComponent } from './modules/categorias/categorias.component';
 
 @NgModule({
   declarations: [
@@ -19,7 +21,9 @@ import { HeaderComponent } from './core/header/header.component';
       LoginComponent,
       DashboardComponent,
       SidebarComponent,
-      HeaderComponent
+      HeaderComponent,
+      IngresosComponent,
+      CategoriasComponent
    ],
   imports: [
     BrowserModule,
@@ -27,10 +31,11 @@ import { HeaderComponent } from './core/header/header.component';
     FormsModule,
     HttpClientModule,
     ReactiveFormsModule,
-    FontAwesomeModule
+    FontAwesomeModule,
+    CommonModule
   ],
-  providers: [LoginService,
-              { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true }
+  providers: [
+    { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true }
   ],
   bootstrap: [AppComponent]
 })
